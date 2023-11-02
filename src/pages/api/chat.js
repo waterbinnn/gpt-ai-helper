401048e847d54e68d409c5b14a141416632b537d
Subin Lee <ewaterbinn@gmail.com>
2023-11-02 18:03:08 +0900

    feat: gpt api server 작성

---
+++ src/pages/api/chat.js
@@ -0,0 +1,23 @@
+import axios from "axios";
+
+export default async function handler(req, res) {
+  if (req.method !== "POST") {
+    res.status(405).json({ message: "Method shuld be POST" });
+  } else {
+    try {
+      const { body } = req;
+      const url = "https://api.openai.com/v1/chat/completions";
+      const headers = {
+        Authorization: `Bearer ${process.env.NEXT_PUBLIC_OPENAI_API_KEY}`,
+        "Content-Type": "application/json",
+      };
+
+      const response = await axios.post(url, body, { headers: headers });
+
+      res.status(200).json(response.data);
+    } catch (err) {
+      console.log(err);
+      res.status(500).json({ message: "error" });
+    }
+  }
+}
