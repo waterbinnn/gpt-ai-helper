76d6b2e62bdb8badb855b8a6c4c2e3ec7b5d94af
Subin Lee <ewaterbinn@gmail.com>
2023-12-15 15:19:51 +0900

    fix: 폴더명 변경

--- src/containers/Agents.tsx
+++
@@ -1,6 +0,0 @@
-import { AgentForm } from "../components";
-import React from "react";
-
-export const Agents = () => {
-  return <AgentForm />;
-};
--- src/containers/GPTChat.tsx
+++
@@ -1,5 +0,0 @@
-import { Chat } from "../components";
-
-export const GPTChat = () => {
-  return <Chat />;
-};
