480c51b5ba941a24e99b012eb9f5260e20c3588e
Subin Lee <ewaterbinn@gmail.com>
2023-12-15 15:31:17 +0900

    style: chat style 수정

--- src/components/Chat/Chat.tsx
+++ src/components/Chat/Chat.tsx
@@ -1,8 +1,8 @@
-import { Message, useChat } from "ai/react";
-import { useCallback, useEffect, useRef } from "react";
+import { Message, useChat } from 'ai/react';
+import { useCallback, useEffect, useRef } from 'react';
 
-import classNames from "classnames/bind";
-import styles from "./Chat.module.scss";
+import classNames from 'classnames/bind';
+import styles from './Chat.module.scss';
 
 const cx = classNames.bind(styles);
 
@@ -17,8 +17,8 @@ export function Chat() {
     if (!textRef.current) {
       return;
     }
-    textRef.current.style.height = "100px";
-    textRef.current.style.height = textRef.current.scrollHeight + "px";
+    textRef.current.style.height = '100px';
+    textRef.current.style.height = textRef.current.scrollHeight + 'px';
   }, []);
 
   useEffect(() => {
@@ -29,65 +29,62 @@ export function Chat() {
   }, [messages]);
 
   return (
-    <main className={cx("chat-container")}>
-      <div className={cx("chat-wrap")}>
-        <ul className={cx("feed")} ref={chatContainerRef}>
+    <main className={cx('chat-container')}>
+      <div className={cx('chat-wrap')}>
+        <ul className={cx('feed')} ref={chatContainerRef}>
+          <li className={cx('chatting')}>
+            <div className={cx('bot-icon')}></div>
+            <p className={cx('chat')}>안녕하세요? 무엇이든 물어보세요! </p>
+          </li>
           {messages.map((message: Message, index: number) => {
             return (
               <li
                 key={message.id + index}
-                className={cx("chatting", { user: message.role === "user" })}
+                className={cx('chatting', { user: message.role === 'user' })}
               >
-                {message.role === "assistant" && (
-                  <div className={cx("bot-icon")}></div>
+                {message.role === 'assistant' && (
+                  <div className={cx('bot-icon')}></div>
                 )}
-                <p
-                  className={cx("chat", {
-                    user: message.role === "user",
-                  })}
-                  key={message.id + index}
-                >
-                  {message.content}
-                </p>
-                {/* <div className={cx("multiple-chats")}>
-                  {message.content
-                    .split("\n")
-                    .map((text: string, index: number) => {
-                      if (text === "") {
-                        return (
-                          <p className={cx("nbsp")} key={message.id + index}>
-                            &nbsp;
-                          </p>
-                        );
-                      } else {
-                        return (
-                          <p
-                            className={cx("chat", {
-                              user: message.role === "user",
-                            })}
-                            key={message.id + index}
-                          >
-                            {text}
-                          </p>
-                        );
-                      }
-                    })}
-                </div> */}
+                {message.role === 'assistant' ? (
+                  <div className={cx('multiple-chats')}>
+                    {message.content
+                      .split('\n')
+                      .map((text: string, index: number) => {
+                        if (text === '') {
+                          return (
+                            <p className={cx('nbsp')} key={message.id + index}>
+                              &nbsp;
+                            </p>
+                          );
+                        } else {
+                          return (
+                            <p className={cx('chat')} key={message.id + index}>
+                              {text}
+                            </p>
+                          );
+                        }
+                      })}
+                  </div>
+                ) : (
+                  <p className={cx('chat', 'user')} key={message.id + index}>
+                    {message.content}
+                  </p>
+                )}
               </li>
             );
           })}
         </ul>
 
-        <form onSubmit={handleSubmit} className={cx("form-wrap")}>
+        <form onSubmit={handleSubmit} className={cx('form-wrap')}>
           <textarea
             ref={textRef}
-            className={cx("message")}
+            className={cx('message')}
             value={input}
             onChange={handleInputChange}
             onInput={handleResizeHeight}
           />
-          <button className={cx("btn-send")}>
-            {isLoading ? "Loading.." : "Send"}
+          <button className={cx('btn-send')}>
+            {isLoading ? 'Loading..' : 'Send'}
           </button>
         </form>
       </div>
